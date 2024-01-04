ef64799ad2f46088d57019efc309777e562cb5c8
Mehedi <132335703+MrDevilEx@users.noreply.github.com>
2024-01-04 16:27:00 +0600

    Update version-x.py

--- version-x.py
+++ version-x.py
@@ -1,7 +1,12 @@
 import os
-import MAHADI32
-MAHADI32.loging()
-#---------64--------#
-import os
-import MAHADI64
-MAHADI64.loging()
+import platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == "32bit":
+    print(" Your Device is 32bit ")
+    print(" Supported ✓ ")
+    __import__("dev32")
+elif bit == "64bit":
+    print(" Your Device is 64bit ")
+    print(" Supported ✓ ")
+    __import__("dev64")
